d74d8ffd65361e3409718098ddbea3a5f88498a6
roctounwvm <roctounwvm@gmail.com>
2026-02-14 15:56:59 -0600

    Add files via upload

---
+++ perikoweb.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+espacio=st.empty()
+
+contraseña="pablocolorcarton"
+
+respuesta=espacio.text_input("contraseña", type="password")
+
+if respuesta == contraseña:
+    espacio.empty()
+    st.title ("bienvenido a la perikoweb")
+    st.text("en esta web encontraras cosas ramdom que encontre en mi galeria")
+
+    if st.button("perro perroso"):
+        st.image("perrofino.jpg") #cambiar despues
+
+    if st.button("oso ososo"):
+        st.image("5fdfa37ae6bd8.jpeg")
+
+    if st.button("gato abogadoso"):
+        st.image("gato.jfif")
+    
+    if st.button("gato huevoso"):
+        st.image("gato huevoso.jpeg")
+
+    if st.button("hmmm jejejhhh auuhhhhhhhh"):
+            st.image("hmmm.jpeg")
+
+    if st.button("peraperosa"):
+         st.image("peraperosa.jfif")
+
+    respuesta2=st.text_input("escribe algo")
+
+    if respuesta2=="bimbo":
+         st.image("bimbo.jfif")
+
+    if respuesta2=="ivan":         
+        st.image("ivan.jfif")
+
+    if respuesta2=="perikoclasico":
+         st.image("perikoclasico.jfif")
+
+    if respuesta2=="recinosavion":
+         st.image("recinosavion.jfif")
+
+    if respuesta2=="yupi":
+         st.image("yupi.jfif")
+
+    
