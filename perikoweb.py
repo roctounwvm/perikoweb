68e8faa0a60e63d489eba37dcb550969e1bd0f6a
roctounwvm <roctounwvm@gmail.com>
2026-08-16 18:14:10 -0600

    Add image display for 'yupi' input
    Added a new condition to display an image when 'yupi' is entered.

--- perikoweb.py
+++ perikoweb.py
@@ -1,18 +1,7 @@
-import streamlit as st
+    if respuesta2 == "yupi":
+        st.image("yupi.jfif")
 
-espacio = st.empty()
-
-contraseña = "pablocolorcarton"
-
-respuesta = espacio.text_input("contraseña", type="password")
-
-if respuesta == contraseña:
-    espacio.empty()
-
-    st.title("bienvenido a la perikoweb")
-    st.text("en esta web encontraras cosas ramdom que encontre en mi galeria")
-
-    # ANUNCIO DEL ARCADE
+    # ANUNCIO AL FINAL DE LA PÁGINA
     st.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
@@ -23,7 +12,7 @@ if respuesta == contraseña:
         color: #00ffff;
         text-align: center;
         text-shadow: 4px 4px #ff00ff;
-        margin: 30px 0;
+        margin: 50px 0 25px 0;
     }
     </style>
 
@@ -36,38 +25,3 @@ if respuesta == contraseña:
         "🎮 ENTRAR AL ARCADE 🎮",
         "https://perikos-arcade.onrender.com/"
     )
-
-    if st.button("perro perroso"):
-        st.image("perrofino.jpg")
-
-    if st.button("oso ososo"):
-        st.image("5fdfa37ae6bd8.jpeg")
-
-    if st.button("gato abogadoso"):
-        st.image("gato.jfif")
-
-    if st.button("gato huevoso"):
-        st.image("gato huevoso.jpeg")
-
-    if st.button("hmmm jejejhhh auuhhhhhhhh"):
-        st.image("hmmm.jpeg")
-
-    if st.button("peraperosa"):
-        st.image("peraperosa.jfif")
-
-    respuesta2 = st.text_input("escribe algo")
-
-    if respuesta2 == "bimbo":
-        st.image("bimbo.jfif")
-
-    if respuesta2 == "ivan":
-        st.image("ivan.jfif")
-
-    if respuesta2 == "perikoclasico":
-        st.image("perikoclasico.jfif")
-
-    if respuesta2 == "recinosavion":
-        st.image("recinosavion.jfif")
-
-    if respuesta2 == "yupi":
-        st.image("yupi.jfif")
